90df40ba2bb610ad4577a39f4323998299e4a15e
John Kim <joimkim@gmail.com>
2016-09-08 10:34:00 -0700

    fix new story popup box

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -5,25 +5,21 @@ $( document ).ready(function() {
     console.log("clicked on image icon")
     $("#pop-up-container").removeClass("hidden");
     $("#pop-up-container").addClass("bounceIn");
-    $("#img_icon").addClass("hidden");
-  });
-
-  $("#img_icon").on("submit", function(event){
-    event.preventDefault();
-    console.log("whattt?")
   });
   
   $("#pop-up").on("submit", function(event){
-  	// var searchTerm = $("#pop-up input:first").val();
+  	var searchTerm = $("#pop-up input:first").val();
   	event.preventDefault();
-  	var input = $(this).serialize();
-  	// api call
-  	$.ajax({
+  	
+    var input = $(this).serialize();
+
+  	var request = $.ajax({
   		method: 'get',
   		url: '/stories/1/images',
   		data: input
   	})
-  	.done(function(response){
+
+  	request.done(function(response){
     
       var root_url = "https://source.unsplash.com/"
       $.each(response, function(index, response) {
@@ -32,17 +28,34 @@ $( document ).ready(function() {
         var my_url = root_url + id + query;
         //get all the cats
         $("#image-list").append(`<li class="list"><div class="images_div"><img id="${id}", class="images_divs_img" src="${my_url}" /></div></li>`);
-      
-        $(".images_divs_img").on("click", function(){
-          var selected_id = this.id;
-          $("#story_image_url").val(selected_id);
-          $(".image_container").css("background-image", `url(${this.src})`);
-          $("#pop-up-container").addClass("bounceOut");
-          $("#img_icon").removeClass("hidden");
-        }); 
+        
+        // $(".images_divs_img").on("click", function(){
+        //   var selected_id = this.id;
+        //   $("#story_image_url").val(selected_id);
+        //   $(".image_container").css("background-image", `url(${this.src})`);
+        //   $("#pop-up-container").addClass("bounceOut");
+        // }); 
       }); 
   	})
   });
+
+  $("#image_pop_up_container").on("click", '.images_divs_img', function(){
+    var selected_id = this.id;
+    $("#story_image_url").val(selected_id);
+    $(".image_container").css("background-image", `url(${this.src})`);
+ 
+        $("#pop-up-container").addClass("bounceOut");
+
+        setTimeout( function(){ 
+          $("#pop-up-container").removeClass("bounceOut");
+          $("#pop-up-container").removeClass("bounceIn");
+          $("#pop-up-container").addClass("hidden");
+        }  , 1000 );
+
+  
+  })
+
+
 
   $(".story_form").on("submit", function(event){
     // event.preventDefault();
